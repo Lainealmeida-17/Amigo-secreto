f03da41519960ed351e57ecea66e130438a9abca
Lainealmeida17 <marialaine2011@hotmail.com>
2025-02-03 17:45:32 -0300

    Adicionando amigos

--- app.js
+++ app.js
@@ -1,4 +1,20 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaAmigos = [];
-let noomeSecreto = gerarNomeAleatorio();
-let tentativas =1;
+
+function adicionarAmigo() {
+    const inputAmigo = document.getElementById('amigo');
+    const nomeAmigo = inputAmigo.value.trim();
+    if (nomeAmigo === '') {
+        alert('Por favor, digite um nome.');
+        return;
+    }
+    if (listaAmigos.includes(nomeAmigo)) {
+        alert('Este nome já foi adicionado.');
+        return;
+    }
+    listaAmigos.push(nomeAmigo);
+    atualizarListaAmigos();
+
+    inputAmigo.value = '';
+} 
+
